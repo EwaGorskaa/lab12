bd48722844ddb93da6c563c1f5421cda3dd0f819
Ewa Górska <ewamgorskaa@gmail.com>
2025-06-05 14:33:03 +0200

    Add index.php and nginx configuration

---
+++ www/index.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LEMP APP</title>
+    <style>
+    body {
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        color: #fff;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        margin: 0;
+        text-align: center;
+    }
+    </style>
+</head>
+<body>
+    <h1>Aplikacja mikrousługowa LEMP</h1>
+    <h4>Laboratorium 12</h4>
+    <p>Ewa Górska</p>
+</body>
+</html>
+
